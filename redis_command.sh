2feb718f67bf78f70d26aae40119995e09597aec
Flying Zhang <13223076623@163.com>
2024-10-18 10:53:41 +0800

    Update redis_command.sh

--- redis_command.sh
+++ redis_command.sh
@@ -5,6 +5,7 @@ puts "特殊字符反斜杠转义"
 set passwd "pw\$d"
 set host "host"
 set port 6379
+set db 1
 
 set timeout 20
 
@@ -12,6 +13,11 @@ spawn redis-cli -h $host -p $port
 sleep 1
 expect "*:$port>"
 send "auth $passwd\r"
+
+sleep 1
+expect "*:$port>"
+send "select $db\r"
+
 
 
 interact
